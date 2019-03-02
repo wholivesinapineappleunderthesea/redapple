de37d6171c13e70ca129f12ca56a93e4dfe6113a
wholivesinapineappleunderthesea <>
2019-03-01 19:01:39 -0500

    K I guess i need to compile elsewhere

--- build/redapple.build/Release/redapple.build/Objects-normal/x86_64/main.d
+++
@@ -1,4 +0,0 @@
-dependencies: /Users/peanut/Documents/redapple/redapple/main.cpp \
-  /Users/peanut/Documents/redapple/redapple/helper.h \
-  /Users/peanut/Documents/redapple/redapple/defs.h \
-  /Users/peanut/Documents/redapple/redapple/CONFIG.h
